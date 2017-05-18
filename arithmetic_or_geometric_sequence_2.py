75d69add4ea3b00b93d29292147c56d3c12469dc
JALoquacious <jalikoudis@protonmail.com>
2017-05-17 23:57:54 -0400

    Added CoderByte Arithmetic_Or_Geometric_Sequence_2 exercise

---
+++ arithmetic_or_geometric_sequence_2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""Using the Python language, have the function ArithGeo(arr) take the array of
+numbers stored in arr and return the string "Arithmetic" if the sequence follows
+an arithmetic pattern or return "Geometric" if it follows a geometric pattern.
+If the sequence doesn't follow either pattern return -1. An arithmetic sequence
+is one where the difference between each of the numbers is consistent, where as
+in a geometric sequence, each term after the first is multiplied by some
+constant or common ratio. Arithmetic example: [2, 4, 6, 8] and Geometric
+example: [2, 6, 18, 54]. Negative numbers may be entered as parameters, 0 will
+not be entered, and no array will contain all the same elements."""
+
+def ArithGeo(array):
+    arithmetic_count = 0
+    geometric_count = 0
+    for i in range(len(array) - 2):
+        if abs(array[i+2] - array[i+1]) == abs(array[i+1] - array[i]):
+            arithmetic_count += 1
+        elif array[i+2] / array[i+1] == array[i+1] / array[i]:
+            geometric_count += 1
+    if arithmetic_count == len(array) - 2:
+        return "Arithmetic"
+    elif geometric_count == len(array) - 2:
+        return "Geometric"
+    else:
+        return -1
+    
+print(ArithGeo([-9,-13,-17,-21])) #"Arithmetic"
+print(ArithGeo([2,9,15,33])) #-1
+print(ArithGeo([2,4,8,16,32,64])) #"Geometric"
+print(ArithGeo([1,1,1,1,1,1,1])) #"Arithmetic" or "Geometric"
