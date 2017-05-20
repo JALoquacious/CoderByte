7a8b2515e109bb252525286828bdacb4b885a05c
JALoquacious <jalikoudis@protonmail.com>
2017-05-19 23:33:26 -0400

    Added unit tests to CoderByte Alphabet_Soup exercise

--- alphabet_soup.py
+++ alphabet_soup.py
@@ -5,17 +5,26 @@ string parameter being passed and return the string with the letters in
 alphabetical order (ie. hello becomes ehllo). Assume numbers and punctuation
 symbols will not be included in the string."""
 
-def AlphabetSoup(string):
+def alphabet_soup(string):
     result = ""
     array = [s for s in string]
-    L = len(array)
-    while L > 0:
+    length = len(array)
+    while length > 0:
         #print(array, result) #debug
         result += min(array)
         array.remove(min(array))
-        L -= 1
+        length -= 1
     return result
 
-print(AlphabetSoup("coderbyte")) #"bcdeeorty"
-print(AlphabetSoup("hooplah")) #"ahhloop"
-print(AlphabetSoup("python")) #"hnopty"
+import unittest
+
+class AlphabetSoupTests(unittest.TestCase):
+    def test_alphabet_soup(self):
+        self.assertEqual(alphabet_soup("coderbyte"), "bcdeeorty")
+        self.assertEqual(alphabet_soup("hooplah"), "ahhloop")
+        self.assertEqual(alphabet_soup("python"), "hnopty")
+        self.assertEqual(alphabet_soup("alphabet"), "aabehlpt")
+        self.assertEqual(alphabet_soup("supercalifragilistic"), "aaccefgiiiillprrsstu")
+        
+if __name__ == '__main__':
+    unittest.main()
