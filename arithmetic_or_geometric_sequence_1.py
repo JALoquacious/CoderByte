6bdb156842d533cff5cb8aeccd38b7ba2e127874
JALoquacious <jalikoudis@protonmail.com>
2017-05-20 12:16:02 -0400

    Added unit tests to CoderByte Arithmetic_or_Geometric_Sequence_1 exercise

--- arithmetic_or_geometric_sequence_1.py
+++ arithmetic_or_geometric_sequence_1.py
@@ -10,16 +10,25 @@ constant or common ratio. Arithmetic example: [2, 4, 6, 8] and Geometric
 example: [2, 6, 18, 54]. Negative numbers may be entered as parameters, 0 will
 not be entered, and no array will contain all the same elements. """
 
-def ArithGeo(array):
+def arith_geo(array):
     difference = abs(array[1] - array[0])
     ratio = array[1] / array[0]
     if abs(array[-1] - array[-2]) == difference:
         return "Arithmetic"
     elif array[-1] / array[-2] == ratio:
         return "Geometric"
-    else: return "-1"
-    
-print(ArithGeo([-9,-13,-17,-21])) #"Arithmetic"
-print(ArithGeo([2,9,15,33])) #-1
-print(ArithGeo([2,4,8,16,32,64])) #"Geometric"
-print(ArithGeo([1,1,1,1,1,1,1])) #(either geometric or arithmatic would be correct)
+    else: return -1
+
+
+import unittest
+
+class AlphabetSoupTests(unittest.TestCase):
+    def test_alphabet_soup(self):
+        self.assertEqual(arith_geo([-9,-13,-17,-21]), "Arithmetic")
+        self.assertEqual(arith_geo([2,9,15,33]), -1)
+        self.assertEqual(arith_geo([2,4,8,16,32,64]), "Geometric")
+        self.assertEqual(arith_geo([1,1,1,1,1,1,1]), "Arithmetic")
+        #(either geometric or arithmatic would be correct)
+        
+if __name__ == '__main__':
+    unittest.main()
