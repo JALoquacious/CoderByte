1738fd8a755482314426d6d4bccab8a131fa9d52
JALoquacious <jalikoudis@protonmail.com>
2017-05-21 16:53:59 -0400

    Added CoderByte Bracket_Matcher exercise

---
+++ bracket_matcher.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""Using the Python language, have the function BracketMatcher(str) take the str
+parameter being passed and return 1 if the brackets are correctly matched and
+each one is accounted for. Otherwise return 0. For example: if str is "(hello
+(world))", then the output should be 1, but if str is "((hello (world))" the
+output should be 0 because the brackets do not correctly match up. Only "(" and
+")" will be used as brackets. If str contains no brackets return 1. """
+
+def BracketMatcher(string):
+    L, R = 0, 0
+    for s in string:
+        if R and not L:
+            return 0
+        if s == "(":
+            L += 1
+        elif s == ")":
+            R += 1
+    return 1 if L == R else 0
+    
+print(BracketMatcher(")((coder)(byte)")) # 0
+print(BracketMatcher("(c(oder)) b(yte)")) # 1
+print(BracketMatcher("((hello (world))")) # 0
