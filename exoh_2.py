27400078d9c0b3dda2c133a0c2016b97a463631e
JALoquacious <jalikoudis@protonmail.com>
2017-06-03 23:47:47 -0400

    Added unit tests to CoderByte ExOh2 exercise

--- exoh_2.py
+++ exoh_2.py
@@ -8,9 +8,17 @@ in the string, no punctuation or numbers. For example: if str is "xooxxxxooxo"
 then the output should return false because there are 6 x's and 5 o's.
 """
 
-def ExOh(string): 
+def ex_oh(string): 
     return True if string.lower().count("x") == string.lower().count("o") else False
     
-print(ExOh("XXXoxOOOxoOXoXxoXOOooxO")) # False (10x,13o)
-print(ExOh("xxXOoo")) # True (3x,3o)
-print(ExOh("ox")) # True (1x,1o)
+import unittest
+
+class ExOhTests(unittest.TestCase):
+    def test_ex_oh(self):
+        self.assertEqual(ex_oh("XXXoxOOOxoOXoXxoXOOooxO"), False)
+        self.assertEqual(ex_oh("xxXOoo"), True)
+        self.assertEqual(ex_oh("ox"), True)
+        self.assertEqual(ex_oh("XXxxOoooO"), False)
+        
+if __name__ == '__main__':
+    unittest.main()
